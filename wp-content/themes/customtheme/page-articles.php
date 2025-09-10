847e7817a3d45f86a804848d91c68edcd4d805e3
NgawangChoedenShankentsang <ngawangchoeden@icloud.com>
2025-09-10 15:50:01 +0200

    articeles pages

--- wp-content/themes/customtheme/page-articles.php
+++ wp-content/themes/customtheme/page-articles.php
@@ -1,6 +1,7 @@
 <?php get_header(); ?>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
+
         <section class="articles wrapper">
             <div class="wrapper__content">
 
@@ -9,6 +10,7 @@
 
                 <?php
                 $posts = get_posts(array(
+                    'numberposts'    => -1,
                     'post_type'         => 'post'
                 ));
 
@@ -23,12 +25,16 @@
                                 <a href="<?php the_permalink(); ?>" class="article__link">
                                     <div class="article__title"><?php the_title(); ?></div>
                                 </a>
+
+
                                 <p class="small article__blurb"> <?php the_excerpt(); ?></p>
+
+
                                 <p class="small article__date"><?php echo get_the_date(); ?></p>
                             </article>
 
                         <?php endforeach; ?>
-
+ 
                         <?php wp_reset_postdata(); ?>
                     </div>
                 <?php endif; ?>
--- wp-content/themes/customtheme/single.php
+++ wp-content/themes/customtheme/single.php
@@ -2,7 +2,20 @@
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 
+        <section class="article wrapper">
+            <div class="wrapper__content">
 
+                <?php the_post_thumbnail('post-thumbnail', ['class' => 'blog__featured-image']); ?>
+
+                <div class="blog__content has-divider">
+                    <h1 class="blog__title"><?php the_title(); ?></h1>
+                    <div class="blog__date"><?php the_date(); ?></div>
+
+                    <?php the_content(); ?>
+                </div>
+
+            </div>
+        </section>
 
 <?php endwhile;
 endif; ?>
